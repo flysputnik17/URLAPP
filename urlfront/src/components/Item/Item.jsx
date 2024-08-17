01b269ddbb2bca46ac755c499bcb867f0200717a
KODKOD177 <pavel2012155@gmail.com>
2024-08-17 20:50:38 +0300

    css minor changes in Itme element

--- urlfront/src/components/Item/Item.jsx
+++ urlfront/src/components/Item/Item.jsx
@@ -1,8 +1,9 @@
 import "./Item.css";
+import "animate.css";
 
 const Item = ({ item }) => {
   return (
-    <div className="item">
+    <div className="item animate__animated animate__fadeInLeft">
       <img className="item__image" src={item.image} alt="item pic"></img>
       <h2 className="item__title">{item.title} </h2>
       <p className="item__description">{item.description}</p>
